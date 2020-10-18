c48980b4f78ac290474d29928caf98f7dde7e09c
Daniel Zawłocki <d.zawlocki@syncraft.pl>
2020-10-18 08:06:31 +0200

    commit

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "SauceCodePro Nerd Font:pixelsize=15:antialias=true:autohint=true";
-/* static char *font = "Neep:pixelsize=18:antialias=true:autohint=true"; */
+/* static char *font = "GohuFont Nerd Font:pixelsize=16:antialias=true:autohint=true"; */
 static char *font2[] = { "DejaVu Sans:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 16;
 
