344173832545cf7a0de301f6fc218cc67fd0145e
Daniel Zawlocki <d.zawlocki@syncraft.pl>
2022-07-05 09:08:09 +0200

    commit

--- config.h
+++ config.h
@@ -5,7 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font:pixelsize=15:antialias=true:autohint=true";
+/* static char *font = "SauceCodePro Nerd Font:pixelsize=13:antialias=true:autohint=true"; */
+static char *font = "SauceCodePro Nerd Font:pixelsize=14:antialias=true:autohint=true";
+/* static char *font = "SauceCodePro Nerd Font:pixelsize=14:antialias=true:autohint=false:hinting=true:hintstyle=hintfull"; */
+/* static char *font = "mononoki Nerd Font Mono:pixelsize=13:antialias=true:autohint=true"; */
 /* static char *font = "GohuFont Nerd Font:pixelsize=14:antialias=true:autohint=true"; */
 static char *font2[] = { "DejaVu Sans:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 8;
