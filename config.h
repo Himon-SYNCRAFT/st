58fda6e703f1704560745d910d4eb5682d7d8367
Daniel Zawlocki <d.zawlocki@syncraft.pl>
2024-03-04 12:29:06 +0100

    commit

--- config.h
+++ config.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "MonaspiceNe NFM Medium:pixelsize=13:antialias=true:autohint=true";
+/* static char *font = "BlexMono Nerd Font Mono Medium:pixelsize=13:antialias=true:autohint=true"; */
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 6;
 
